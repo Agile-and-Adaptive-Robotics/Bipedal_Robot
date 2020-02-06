e4005d75360d3fc7cda1e8f90333ca5b191be0a5
Connor Morrow <conroy135@gmail.com>
2020-02-06 13:47:25 -0800

    Updated Pam locations to be generic
    Updated pam locations to be generic, for ease of updating in the optimization code. Also added a flag to turn on or off calculations depending on if they have already been calced.

--- Code/BipedalMaster.m
+++ Code/BipedalMaster.m
@@ -16,7 +16,7 @@ end
 
 %Choose the Joint that you will to observe
 %Options are: Back, Bi_Hip, Calves, Foot, Toe, Uni_Hip
-ChooseJoint = 'Uni_Hip';
+ChooseJoint = 'Back';
 
 % %Choose the number of divisions for the angles of rotation
 divisions = 100;
@@ -38,7 +38,8 @@ load(strcat('Human_', ChooseJoint, '_Data.mat'));
 
 %% ------------- Robot Model -----------------
 %Runs the bipedal model
-run("RobotPAMCalculation.m")    
+run("RobotPAMCalculationOptimization.m")    
+% run("RobotPAMCalculation.m")    
 
 %% ------------- Error Caclulation ------------
 %After running both scripts, this portion checks to see the error between
