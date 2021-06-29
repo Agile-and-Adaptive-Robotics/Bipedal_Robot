afdbcb0f1c31879d3e130762b2ee76c0427ce88d
Aidan Vogt <70345765+AidanVogt@users.noreply.github.com>
2021-06-29 16:52:19 -0700

    adding pressure sensor functionality
    Edited matlab file to store pressure sensor data (untested)

--- Code/Matlab/KneeTest.m
+++ Code/Matlab/KneeTest.m
@@ -27,14 +27,15 @@ total = 6000;
   %for protocol_id == '1', total == 150
   %for protocol_id == '2', total needs to be at least 6000 for the system
   %to reach equilibrium
-svalues = cell (total,2);
+svalues = cell (total,3);
 
 tic %start timer
 for i = 1:total
 
-       svalues{i,1} = fgetl(s); %read information from buffer into column 1
-       svalues{i,2} = toc; %put a timestamp in column 2 for the information
-       % read into column 1 
+       svalues{i,1} = fgetl(s); %read info from load cell into column 1
+       svalues{i,2} = fget1(2); %read info from pressure censor into column 2
+       svalues{i,3} = toc; %put a timestamp in column 3 for the information
+       % read into columns 1 and 2
 end
 
 
@@ -42,8 +43,9 @@ end
 %svalues into a numeric array
 a = 1;
 while ~isempty(svalues{a}) && a<total
-        data(a,1) = str2num(svalues{a})*4.4482216; %%Convert lb to N
-        data(a,2) = svalues{a,2};
+        data(a,1) = str2num(svalues{a,1})*4.4482216; %%Convert lb to N
+        data(a,2) = str2num(svalues{a,2});
+        data(a,3) = svalues{a,3};
         a = a+1;
 end
 
@@ -52,11 +54,18 @@ end
 Data = data;
 
 %Create a Force vs. time plot with the information in Data
-Y = data(:,1);
-X = data(:,2);
-plot(X,Y,'.')
-xlabel('Time (sec)')
-ylabel('Force(N)')
+Force = data(:,1);
+X = data(:,3);
+yyaxis left
+xlabel('Time (sec)');
+ylabel('Force(N)');
+plot(X,Force,'.');
+
+%Graph pressure against right y axis
+Pressure = data(:,2);
+yyaxis right
+ylabel('Pressure( )');
+plot(X,Pressure,'.');
 
 %Use the following bit of code to find some basic statistics about the data
 %that has been collected.  The operating assumption here is that collecting 
@@ -66,11 +75,19 @@ ylabel('Force(N)')
 
 % **NOTE: If protocol_id == '1', either comment out the following code,
 % or change the range of data on which the basic statistics are calculated.
-Stats = zeros(4,1);
+
+%Force stats
+Stats = zeros(4,2);
 Stats(1,1) = mean(data(5500:5999,1));
 Stats(2,1) = median(data(5500:5999,1));
 Stats(3,1) = mode(data(5500:5999,1));
 Stats(4,1) = std(data(5500:5999,1));
+
+%Pressure stats
+Stats(1,2) = mean(data(5500:5999,2));
+Stats(2,2) = median(data(5500:5999,2));
+Stats(3,2) = mode(data(5500:5999,2));
+Stats(4,2) = std(data(5500:5999,2));
 Stats;
 
 fclose(s)
