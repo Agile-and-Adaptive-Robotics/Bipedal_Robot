15c914f7eded0d5f62514621b10ad4385822b162
Aidan Vogt <70345765+AidanVogt@users.noreply.github.com>
2021-07-16 09:43:31 -0700

    updated commenting/formatting

--- Code/Arduino/KneeTorqueTest/KneeTorqueTest.ino
+++ Code/Arduino/KneeTorqueTest/KneeTorqueTest.ino
@@ -1,104 +1,109 @@
-/* sketch adapted by Lindie Burgess for use by the Agile and Adaptive Robotics Lab at Portland State
-  University.
+/*sketch adapted by Lindie Burgess for use by the Agile and Adaptive Robotics Lab at Portland State
+University.
 
-  This sketch is used in conjunction with Matlab function called, "KneeTest.m"
+This sketch is used in conjunction with Matlab function called, "KneeTest.m"
 
-  This arduino sketch is adapted from the "SparkFun_HX711_KnownZeroStartup" example, which
-  can be found here
-  https://github.com/sparkfun/HX711-Load-Cell-Amplifier/tree/master/firmware
+This arduino sketch is adapted from the "SparkFun_HX711_KnownZeroStartup" example, which
+can be found here
+https://github.com/sparkfun/HX711-Load-Cell-Amplifier/tree/master/firmware
 
-  Source Code for the HX711 can be found here:
-  https://github.com/bogde/HX711/tree/master/src
+Source Code for the HX711 can be found here:
+https://github.com/bogde/HX711/tree/master/src
 
+Example using the SparkFun HX711 breakout board with a scale
+By: Nathan Seidle
+SparkFun Electronics
+Date: November 19th, 2014
+License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
 
-  Example using the SparkFun HX711 breakout board with a scale
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: November 19th, 2014
-  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+Most scales require that there be no weight on the scale during power on. This sketch shows how to pre-load tare values
+so that you don't have to clear the scale between power cycles. This is good if you have something on the scale
+all the time and need to reset the Arduino and not need to tare the scale.
 
-  Most scales require that there be no weight on the scale during power on. This sketch shows how to pre-load tare values
-  so that you don't have to clear the scale between power cycles. This is good if you have something on the scale
-  all the time and need to reset the Arduino and not need to tare the scale.
+This example code uses bogde's excellent library: https://github.com/bogde/HX711
+bogde's library is released under a GNU GENERAL PUBLIC LICENSE
 
-  This example code uses bogde's excellent library: https://github.com/bogde/HX711
-  bogde's library is released under a GNU GENERAL PUBLIC LICENSE
+The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
+based load cell which should allow a user to measure everything from a few grams to tens of tons.
+Arduino pin 2 -> HX711 CLK
+3 -> DOUT
+5V -> VCC
+GND -> GND
 
-  The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
-  based load cell which should allow a user to measure everything from a few grams to tens of tons.
-  Arduino pin 2 -> HX711 CLK
-  3 -> DOUT
-  5V -> VCC
-  GND -> GND
+The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.*/
 
-  The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+#include "HX711.h" //this library can be obtained here http://librarymanager/All#Avia_HX711
 
-*/
-
-
-#include "HX711.h" //This library can be obtained here http://librarymanager/All#Avia_HX711
-
-#define LOADCELL_DOUT_PIN 3  //define the Serial Data Output Pin
-#define LOADCELL_SCK_PIN 2  // define the Power Down and Serial Clock Input Pin
+#define LOADCELL_DOUT_PIN 3   //define the Serial Data Output Pin
+#define LOADCELL_SCK_PIN 2    //define the Power Down and Serial Clock Input Pin
 
 HX711 scale;
 
-int choose_branch; // initialize the variable "choose_branch"
+int choose_branch; //initialize the variable "choose_branch"
 float calibration_factor = -15400;
-// initialized variable calibration_factor.
-// Calibration factor found using a separate arduino sketch
-// called SLoadCell_CalibrationFactorSketch"
+//initialized variable calibration_factor.
+//calibration factor found using a separate arduino sketch
+//called SLoadCell_CalibrationFactorSketch"
 
 
 void setup() {
   pinMode(4, OUTPUT);
-  Serial.begin(115200);  // initialize arduino serial communication
+  Serial.begin(115200);  //initialize arduino serial communication
   Serial.setTimeout(1);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN); // initialize load cell
+  
+  //initialize load cell
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN); 
   scale.set_scale(calibration_factor);
-  //set the scale value;
+  //set the scale value
   //this value is used to convert the raw
   //data to "human readable" data.  Output units are in lbs
+  
   scale.set_offset(-17200);
-  //This sets the offset value to a known zero.
-  //There is no need for taring the scale once the zero point is known for a scale in a
-  // set configuration.
-  // The Zero Factor -17200 was found using a separate Arduino Sketch
+  //this sets the offset value to a known zero.
+  //there is no need for taring the scale once the zero point is known for a scale in a
+  //set configuration.
+  //the Zero Factor -17200 was found using a separate Arduino Sketch
   //called "SLoadCell_ZeroFactorSketch"
 }
 
 void loop() {
   char choose_branch = '0';
   int total = 0;
-  if (Serial.available() > 0) {     // if information is sent over serial from matlab]
-    choose_branch = Serial.read();  // read the serial data into variable "choose_branch"
-    if (choose_branch == '2') {     // If choose_branch is equal to '2', iterate through the following for loop
+  
+  if (Serial.available() > 0) {     //if information is sent over serial from matlab]
+    choose_branch = Serial.read();  //read the serial data into variable "choose_branch"
+    if (choose_branch == '2') {     //if choose_branch is equal to '2', iterate through the following for loop
 
-      Serial.println("running"); // tell matlab that the arduino recieved the protocol id
+      Serial.println("running");    // tell matlab that the arduino recieved the protocol id
       String reading = "2";
-      // initiate variable to store the serial data when it is being read
-
+      //initiate variable to store the serial data when it is being read
 
       while (true) {
-        reading = Serial.readString();                    //read from serial
-        //if the reading is anything but 2 then break and start collecting data (we need to ignore 2 because matlab sends many instances
-        if (reading.length() > 1 and reading != "2\n") {  //of the protocol id, so we want to make sure that it checks for the next unique value, which would be how many data points to collect)
+        reading = Serial.readString();                      
+        //read from serial
+        
+        //if the reading is anything but 2 then break and start collecting data (we need to ignore any value of '2' because matlab sends many instances   
+        //of the protocol id, so we want to make sure that it checks for the next unique value, which would be how many data points to collect)
+        if (reading.length() > 1 and reading != "2\n") {    
           break;
         }
       }
 
       total = reading.toInt();     //convert the read string to an integer
       double start = millis();     //start timer
+      
       for (int i = 0; i < total; i++) {
+        
         digitalWrite(4,HIGH);
-        Serial.println(scale.get_units(), 1); //scale.get_units() returns a float representing the force on the load cell
-        Serial.println(analogRead(A0));       //reads raw pressure sensor data
-        Serial.println(millis() - start);     //record time stamp of data collection
+        Serial.println(scale.get_units(), 1);   //scale.get_units() returns a float representing the force on the load cell
+        Serial.println(analogRead(A0));         //reads raw pressure sensor data
+        Serial.println(millis() - start);       //record time stamp of data collection
+        
       }
       digitalWrite(4, LOW);
-    
-  } else {  //if there is no information to read over serial from matlab, wait...
-  digitalWrite(4,LOW );
+      
+    } else {  //if there is no information to read over serial from matlab, wait...
+      digitalWrite(4,LOW );
+    }
   }
-}
 }
--- Code/Matlab/KneeTest.m
+++ Code/Matlab/KneeTest.m
@@ -1,23 +1,26 @@
 %This function is written to be used with 2 arduino sketches
-%(1) SLoadCell_ZeroFactorSketch, when protocol_id == '1'
-%(2) Knee TorqueTest, when protocol_id == '2'
-%if protocol_id == '2', then the 'total' variable is how many data points
-%to collect
-%Minor Adjustments are needed to this function depending on which Arduino 
-%sketch it is being used with
+%(1) SLoadCell_ZeroFactorSketch, when protocol_id == 1
+%(2) Knee TorqueTest, when protocol_id == 2
+%if protocol_id == 2, then the 'total' variable is how many data points
+%to collect (should be at least 10000 to get reliable data)
 
+function [Data, Stats] = KneeTest(protocol_id,port,varargin) 
 
-function [Data, Stats] = KneeTest(protocol_id, total) 
-
-    %for protocol_id == '1', total == 150
-    %for protocol_id == '2', total needs to be at least 6000 for the system
-    %to reach equilibrium
-
-
-    format short g      %remove scientific notation
+    p = inputParser;
+    addRequired(p,'protocol_id');
+    addRequired(p,'port');
+    addOptional(p,'total',50000);
+    parse(p,protocol_id,port,varargin{:});
+    
+    protocol_id = num2str(p.Results.protocol_id);
+    port = strjoin({'COM',num2str(p.Results.port)},'');
+    total = num2str(p.Results.total);
+    
+    %remove scientific notation
+    format short g
 
     %Initialize serial port 
-    s = serialport('COM5',115200);
+    s = serialport(port,115200);
 
     while s.NumBytesAvailable < 1               
         write(s,string(protocol_id),'string');
@@ -26,115 +29,117 @@ function [Data, Stats] = KneeTest(protocol_id, total)
     %protocol_id, which increases the value of s.NumBytesAvailable and 
     %breaks the loop
 
-    if protocol_id == 2   %the rest is for the knee torque test
+    write(s,string(total),'string')
+    readline(s)
+    %writes the amount of data points to collect, reads once to clear
+    %the "running" string from the buffer before reading the data
 
-        write(s,string(total),'string')
-        readline(s)
-        %writes the amount of data points to collect, reads once to clear
-        %the "running" string from the buffer before reading the data
+    svalues = zeros(total,3);   %creates array for the data
+    clf;                        %clears graph from any previous tests
 
+    yyaxis left     %graph force on left axis in blue
+    Force = animatedline('color','blue');
+    ylim([-50,300]);
+    ylabel('Force (N)');
 
-        svalues = zeros(total,3);   %creates array for the data
-        clf;                        %clears graph from any previous tests
+    yyaxis right    %graph pressure on right in red
+    Pressure = animatedline('color','red');
+    ylim([10,700]);
+    ylabel('Pressure (kPa)');
+
+    %the load cell data will sometimes spike unexpectedly which
+    %causes problems in data collection
+
+    prev = 1;   
+    %since the spikes can span over multiple data readings, 
+    %this variable keeps track of the last data point not in the spike 
+    %and compares new data against it until it finds one that is within
+    %100 N, which indicates that the spike has ended
+
+    for i = 1:total
         
-        yyaxis left     %graph force on left axis in blue
-        Force = animatedline('color','blue');
-        ylim([-50,300]);
-        ylabel('Force (N)');
+        svalues(i,1) = str2double(readline(s))*4.4482216;                                
+        svalues(i,2) = str2double(readline(s))*395/512-115;
+        svalues(i,3) = str2double(readline(s))/1000;
 
-        yyaxis right    %graph pressure on right in red
-        Pressure = animatedline('color','red');
-        ylim([10,700]);
-        ylabel('Pressure (kPa)');
+        %read data to each column and convert units when needed
+        %column 1 is force, converting lbs to N
+        %column 2 is pressure, converting analog value (1-1023) to
+        %kPa
+        %column 3 is the time that the data was collected,
+        %converting milliseconds to seconds
 
-        %the load cell data will sometimes spike unexpectedly which
-        %causes problems in data collection
-
-        prev = 1;   
-        %since the spikes can span over multiple data readings, 
-        %this variable keeps track of the last data point not in the spike 
-        %and compares new data against it until it finds one that is within
-        %100 N, which indicates that the spike has ended
-
-        for i = 1:total
-                svalues(i,1) = str2double(readline(s))*4.4482216;                                
-                svalues(i,2) = str2double(readline(s))*395/512-115;
-                svalues(i,3) = str2double(readline(s))/1000;
-
-                %read data to each column and convert units when needed
-                %column 1 is force, converting lbs to N
-                %column 2 is pressure, converting analog value (1-1023) to
-                %kPa
-                %column 3 is the time that the data was collected,
-                %converting milliseconds to seconds
-
-            if i > 1
+        if i > 1
+            
             %i > 1 because the spike detection references a previous i
             %value, which doesn't exist if the loop started at the minimum
             %i value
+            
                 if abs(svalues(i-prev,1)-svalues(i,1))<100     
-                %check if next value is more than
-                %100 N away from the last known value
+                    
+                    %check if next value is more than
+                    %100 N away from the last known value
                     prev = 1;
                     addpoints(Force,svalues(i,3),svalues(i,1)); 
                     %if so, add the force data
+                    
                 else
+                    
                     svalues(i,1) = svalues(i-prev,1);   
                     %otherwise, replace it with the last known data point
+                    
                     disp('spike');                      
                     disp(svalues(i,3));
                     prev = prev+1;                        
                     %increase the amount of points since 
                     %the last value outside of the spike
+                    
                 end                                     
 
-
-                addpoints(Pressure,svalues(i,3),svalues(i,2));  
-                %add pressure and time data
-            end
+            addpoints(Pressure,svalues(i,3),svalues(i,2));  
+            %add pressure and time data
+            
         end
-
-        addpoints(Force,svalues(i,3),svalues(i,1));     %add data to graph
-        addpoints(Pressure,svalues(i,3),svalues(i,2));
-        drawnow  
-
-
-        %Use the following bit of code to find some basic statistics about the data
-        %that has been collected.  The operating assumption here is that collecting 
-        %6000 data points will be enough for the system to have reached equilibriam,
-        %and that the last 500 data points will exists outside of the transient
-        %system response.
-
-        %**NOTE: If protocol_id == '1', either comment out the following code,
-        %or change the range of data on which the basic statistics are calculated.
-        
-
-        forceData = svalues(500:total,1);
-        pressureData = svalues(500:total,2);
-
-        %Force stats
-        stats = zeros(6,2);
-        stats(1,1) = mean(forceData);
-        stats(2,1) = median(forceData);
-        stats(3,1) = mode(forceData);
-        stats(4,1) = min(forceData);
-        stats(5,1) = max(forceData);
-        stats(6,1) = std(forceData);
-
-        %Pressure stats
-        stats(1,2) = mean(pressureData);
-        stats(2,2) = median(pressureData);
-        stats(3,2) = mode(pressureData);
-        stats(4,2) = min(pressureData);
-        stats(5,2) = max(pressureData);
-        stats(6,2) = std(pressureData);
-
-        rows = {'Mean','Median','mode','min','max','Standard Deviation'};
-        columns = {'Force','Pressure'};
-
-        stats = array2table(stats,'RowNames',rows,'VariableNames',columns);
-       
     end
-Data = svalues;
-Stats = stats;
+
+    addpoints(Force,svalues(i,3),svalues(i,1));     %add data to graph
+    addpoints(Pressure,svalues(i,3),svalues(i,2));
+    drawnow  
+
+    %Use the following bit of code to find some basic statistics about the data
+    %that has been collected.  The operating assumption here is that collecting 
+    %6000 data points will be enough for the system to have reached equilibriam,
+    %and that the last 500 data points will exists outside of the transient
+    %system response.
+
+    %**NOTE: If protocol_id == '1', either comment out the following code,
+    %or change the range of data on which the basic statistics are calculated.
+
+    forceData = svalues(500:total,1);
+    pressureData = svalues(500:total,2);
+
+    %Force stats
+    stats = zeros(6,2);
+    stats(1,1) = mean(forceData);
+    stats(2,1) = median(forceData);
+    stats(3,1) = mode(forceData);
+    stats(4,1) = min(forceData);
+    stats(5,1) = max(forceData);
+    stats(6,1) = std(forceData);
+
+    %Pressure stats
+    stats(1,2) = mean(pressureData);
+    stats(2,2) = median(pressureData);
+    stats(3,2) = mode(pressureData);
+    stats(4,2) = min(pressureData);
+    stats(5,2) = max(pressureData);
+    stats(6,2) = std(pressureData);
+
+    rows = {'Mean','Median','mode','min','max','Standard Deviation'};
+    columns = {'Force','Pressure'};
+    stats = array2table(stats,'RowNames',rows,'VariableNames',columns);
+
+    Data = svalues;
+    Stats = stats;
+
 end
