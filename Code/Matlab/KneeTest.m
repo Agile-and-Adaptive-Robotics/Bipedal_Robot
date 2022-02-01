add4c0d7b321b9362794135e25048c284e702d2f
bbolen83 <59748829+bbolen83@users.noreply.github.com>
2022-02-01 00:02:40 -0800

    Finished 10mm flexor testing
    collected data for 10 mm Festo flexor testing.

--- Code/Matlab/KneeTest.m
+++ Code/Matlab/KneeTest.m
@@ -67,7 +67,7 @@ function [Data, Stats] = KneeTest(protocol_id,port,varargin)
     for i = 1:total
         
         svalues(i,1) = str2double(readline(s))*4.4482216;                                
-        svalues(i,2) = str2double(readline(s))*152.45*(5/1023)-121.21;
+        svalues(i,2) = str2double(readline(s))*154.48*(5/1023)-124.86;
         svalues(i,3) = str2double(readline(s))/1000;
 
 %         read data to each column and convert units when needed
