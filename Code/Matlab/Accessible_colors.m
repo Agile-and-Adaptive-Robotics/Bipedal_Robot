fbddfb5668f05003c9d3b052a18f80e0c6fdad12
bbolen83 <59748829+bbolen83@users.noreply.github.com>
2022-04-01 11:55:57 -0700

    created color palette
    created color palette to have color accessible to people with R-G colorblindness

---
+++ Code/Matlab/Accessible_colors.m
@@ -0,0 +1,11 @@
+%% Colors
+%Matlab hex values for colors that remain accessible to people with
+%colorblindness
+
+c1 = '#FFD700'; %gold
+c2 = '#FFB14E'; %orange
+c3 = '#FA8775'; %light orange
+c4 = '#EA5F94'; %pink
+c5 = '#CD34B5'; %magenta
+c6 = '#9D02D7'; %magenta 2
+c7 = '#0000FF'; %indigo
