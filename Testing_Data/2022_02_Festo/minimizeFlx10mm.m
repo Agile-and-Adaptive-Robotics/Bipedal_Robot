1fc664c202dc5a3e40bb2051a6770c28a69da813
bbolen83 <59748829+bbolen83@users.noreply.github.com>
2022-04-21 17:09:53 -0700

    Trying to create optimization function
    Function is based on the simplex search method.  Matlab script isn't executing because "Unable to perform assignment because the size of the left side is 1-by-1 and the size of the right side is 1-by-10." error in fminsearch line 200.

--- Testing_Data/2022_02_Festo/minimizeFlx10mm.m
+++ Testing_Data/2022_02_Festo/minimizeFlx10mm.m
@@ -1,62 +1,15 @@
-function [A,I,M, G, M2] = minimizeFlx10mm
+
 load KneeFlx_10mm_42cm.mat TorqueR phiD Location Name kmax CrossPoint Dia T_Pam fitting
 Theoretical = TorqueR(:,3)';   %Load theoretical torque
 load Plot_KneeFlx_10mm_42cm.mat X1 Angle Torque modp mdl1 gofp2 TorqueMean pres 
 pres = mean(pres);                  %Make pressure a scalar value
-y1 = feval(mdl1, X1);               %Make y1 the curve fit
-%y1 = Torque                        %Make it just the data
-rest = 0.415;
-tendon = 0.012;
-kmax = 0.350;
-%KMAX = kmax;
-deltaRest = (-10:20)/1000;
-deltaTendon = (-5:30)/1000;
-deltaK = (-10/10)/1000;
-A = NaN(length(deltaTendon),length(deltaRest),length(deltaK));
-%A = NaN(length(deltaTendon),length(deltaRest));
+y1 = Torque;                        %Make y1 the data points
+y = feval(mdl1, X1);                %Make y the curve fit
+y3 = diff(y);                       %y3 is the derivative of the curve fit
 
-  for t = 1:length(deltaTendon)         %vary the tendon length
-    for u = 1:length(deltaRest)         %vary the resting length
-        for v = 1:length(deltaK)       %vary the kmax length
-            restingL = rest+deltaRest(u);
-            Tendon = tendon+deltaTendon(t);
-            KMAX = kmax+deltaK(v);
-            y2 = nestedfun1(restingL,Tendon,KMAX);
-            yresid = y1-y2;                     %residual error
-            SSresid = sum(yresid.^2);            %Sum of squares of the residual
-            %SStotal = (length(y1)-1)*var(y1);   %total sum of squares
-            A(t,u,v) = SSresid;
-            G(v) = min(A,[],[1 2]);
-        end
-    end
-  end
-
-Gdisp = G
-Afinal = A
-[M,I] = min(A,[],'all','linear');         %M finds the minimum value of A over all dimensions. I is the index location.
-M2 = min(A,[],'all');
-M
-I
-[Irow, Icolumn, I3] = ind2sub([length(deltaTendon),length(deltaRest), length(deltaK)],I)
-
-    function val = nestedfun1(restingL, Tendon, KMAX)
-        % Find the difference between experimental and calculated results
-        % Inputs:
-        %Rest = resting length, meters
-        %tendon = tendon length, meters
-        %kmax = maximum contracted length, meters (will be converted to percent later)
-        Rest = restingL;
-        kmax1 = KMAX;
-        Teender = Tendon; 
-%         pres = mean(pres);         %average pressure, make sure 
-        Bifemsh_Pam_adj = MonoPamDataExplicit(Name, Location, CrossPoint, Dia, T_Pam, Rest, kmax1, Teender, fitting, pres); %Call the class to do the Torque calculation
-        T2 = Bifemsh_Pam_adj.Torque(:,3);             %Calculated Adjusted Torque
-        mod_Pam = fittype('smoothingspline');
-        Options = fitoptions(mod_Pam);
-        Options.Normal = 'on';
-        Options.Exclude = isnan(T2);
-        mdl_Pam = fit(phiD',T2,mod_Pam,Options);
-        val = feval(mdl_Pam,X1);
-    end
-
-end
+rest = 0.415;                       %x(1), measured
+tendon = 0.012;                     %x(2), guess
+kmax = 0.350;                       %x(3), measured
+x0 = [rest, tendon, kmax];
+c = [1 10];                         %c1 is value of importance for matching data points, c2 is value of importance for matching slope of fitted lines
+[X, FVAL] = minimizeFlx10mm_nest(x0,X1,Name, Location, CrossPoint, Dia, T_Pam, fitting, pres, phiD, y1, y3, c)
---
+++ Testing_Data/2022_02_Festo/minimizeFlx10mm_nest.m
@@ -0,0 +1,41 @@
+   function [x, fval] = minimizeFlx10mm_nest(x0,X1,Name, Location, CrossPoint, Dia, T_Pam, fitting, pres, phiD, y1, y3, c)
+
+    options = optimset('Display','iter','PlotFcns',@optimplotfval);
+    [x, fval] = fminsearch(@minimize,x0,options);
+
+        function f = minimize(x)
+
+
+        [y2, y4] = nestedfun1(x);
+        yresid1 = y1-y2;                     %residual error
+        SSresid1 = sum(yresid1.^2);          %Sum of squares of the residual
+        %SStotal = (length(y1)-1)*var(y1);   %total sum of squares
+
+        yresid2 = y3-y4;                     %residual error from derivatives
+        SSresid2 = sum(yresid2.^2);          %Sum of squares of the residual from the derivatives
+
+        f = c(1)*SSresid1+c(2)*SSresid2;     %Combine the error from the data and the error of the slopes
+
+
+            function [val, der] = nestedfun1(x)
+            % Find the difference between experimental and calculated results
+            % Inputs:
+            %Rest = resting length, meters
+            %tendon = tendon length, meters
+            %kmax = maximum contracted length, meters (will be converted to percent later)
+            Rest = x(1);
+            kmax1 = x(2);
+            Teender = x(3); 
+            Bifemsh_Pam_adj = MonoPamDataExplicit(Name, Location, CrossPoint, Dia, T_Pam, Rest, kmax1, Teender, fitting, pres); %Call the class to do the Torque calculation
+            T2 = Bifemsh_Pam_adj.Torque(:,3);             %Calculated Adjusted Torque
+            mod_Pam = fittype('smoothingspline');
+            Options = fitoptions(mod_Pam);
+            Options.Normal = 'on';
+            Options.Exclude = isnan(T2);
+            mdl_Pam = fit(phiD',T2,mod_Pam,Options);
+            val = feval(mdl_Pam,X1);
+            der = diff(val);
+            end
+
+    end
+end
