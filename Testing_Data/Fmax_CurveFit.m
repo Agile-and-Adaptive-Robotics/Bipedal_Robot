2f11554ab8405fdeca6c3efc434dbd454993797f
bbolen83 <59748829+bbolen83@users.noreply.github.com>
2022-09-28 18:54:48 -0700

    Create Fmax_CurveFit.m
    Create script to curve fit equations Fmax(restingL) and kmax(restingL). kmax does not appear to be function of resting length. Fmax appears to be a function with a limit.  Good candidates for fit are using atan or tanh. Need to play around with options to get  best statistics.

---
+++ Testing_Data/Fmax_CurveFit.m
@@ -0,0 +1,34 @@
+%% Look for equations of max force and max strain as functions of resting length
+%  Data from experiments. See Excel spreadsheet "StraightForceTest"
+
+clear; clc; close all
+
+restingL = [845	840	785	780	710	709	571	571	112	415	455	490	518	551	361	54	27	69	275	151	193];
+
+kmax = [0.171597633	0.172619048	0.169426752	0.170512821	0.177464789	0.174894217	0.183887916	0.182136602	0.160714286	0.16626506	0.158241758	0.187755102	0.166023166	0.174228675	0.171745152	0.148148148	0.148148148	0.15942029	0.170909091	0.152317881	0.165803109];
+
+Fmax = [447.1	447.1	472	472	452.32	452.32	461.6	461.6	334	445	464.5	458.6	490	453.14	436.4	238.2	135.32	271.48	412.3	347.18	396.17];
+
+%% Nonlinear fit for Fmax
+modelfun = @(b,x)b(1)*tanh(b(2)*x+b(3))+b(4); %based on the shape, it looks like resting length reaches a limit
+beta0 = [300 0.0286 0 0];
+mdl = fitnlm(restingL,Fmax,modelfun,beta0)
+x = linspace(0,1000);
+p1 = feval(mdl,x);
+
+figure
+plot(restingL,Fmax,'o',x,p1,'--')
+xlabel('Resting Length, mm')
+ylabel('Maximum Force, N')
+legend('Data','Model Fit')
+
+%% Fit for maximum strain
+
+max_k = fit(restingL',kmax','poly2','Normalize','on')
+p2 = feval(max_k,x);
+
+figure
+plot(restingL,kmax,'o',x,p2,'--')
+xlabel('Resting Length, mm')
+ylabel('Maximum Strain')
+legend('Data','Model Fit')
