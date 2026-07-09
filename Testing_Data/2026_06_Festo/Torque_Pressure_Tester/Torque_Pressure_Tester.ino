295e46ed6b446b1712be30d850b9e0e2835e06b3
Mohammad Elzein <melzein1@hotmail.com>
2026-07-09 16:59:38 -0700

    test_code_updates

---
+++ Testing_Data/2026_06_Festo/Torque_Pressure_Tester/Torque_Pressure_Tester.ino
@@ -0,0 +1,34 @@
+#include "HX711.h"
+
+HX711 scale;
+
+const uint8_t DATA_PIN  = 3;
+const uint8_t CLOCK_PIN = 2;
+const int sensorPin = A0; // Analog Input from MPX5700GP Pin 1
+// Replace these with your printed calibration values
+const int32_t OFFSET = -12228;
+const float SCALE_FACTOR = -4489.861328;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  scale.begin(DATA_PIN, CLOCK_PIN);
+
+  scale.set_offset(OFFSET);
+  scale.set_scale(SCALE_FACTOR);
+}
+
+void loop()
+{
+  float forceN = scale.get_units(1);
+  float pressureRaw = analogRead(sensorPin);
+  float pressureCalibrated = 0.7633 * pressureRaw - 13.744;
+  Serial.print("Force = ");
+  Serial.print(forceN, 3);
+  Serial.print(" N ");
+  Serial.print("Pressure = ");
+  Serial.print(pressureCalibrated, 3);
+  Serial.println(" kPa ");
+  //delay(10);
+}
